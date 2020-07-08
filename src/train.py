47d92c3f0e9d21b5ec252ddcf4d2abfc5d7c1f43
noname <MY_NAME@example.com>
2020-07-08 19:12:26 +0200

    feature/ compute val loss

--- src/train.py
+++ src/train.py
@@ -1,39 +1,80 @@
+import os
+import argparse
 import torch
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
-import argparse
-import os
+from sklearn.model_selection import train_test_split
 
 from src.model import Net
 from src.experiment_setup import ex
 from src.utils import save_model
 
 
-@ex.automain
-def main(_run):
-    args = argparse.Namespace(**_run.config)
-    train_sampler = None
+def compute_val(model, val_loader, criterion):
+    model.eval()
+    val_loss = 0.
+    for _, data in enumerate(val_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
 
+        # forward
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        # print statistics
+        val_loss += loss.item()
+
+    return val_loss / len(val_loader)
+
+
+def load_data(args):
     if args.dataset == "CIFAR10":
         transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        trainset = torchvision.datasets.CIFAR10(root=args.root, train=True,
-                                                download=True, transform=transform)
+        train_val = torchvision.datasets.CIFAR10(root=args.root, train=True,
+                                                 download=True, transform=transform)
     else:
         raise NotImplementedError
 
+    # Train-Val split
+    targets = train_val.targets
+    train_idx, val_idx = train_test_split(
+        range(len(targets)),
+        test_size=args.valsize,
+        shuffle=True, stratify=targets)
+
+    train_set = torch.utils.data.Subset(train_val, train_idx)
+    val_set = torch.utils.data.Subset(train_val, val_idx)
     train_loader = torch.utils.data.DataLoader(
-        trainset,
+        train_set,
         batch_size=args.batch_size,
-        shuffle=(train_sampler is None),
+        shuffle=True,
         drop_last=True,
         num_workers=args.workers,
-        sampler=train_sampler,
+        sampler=None,
     )
+    val_loader = torch.utils.data.DataLoader(
+        val_set,
+        batch_size=args.batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=args.workers,
+        sampler=None,
+    )
+
+    return train_loader, val_loader
+
+
+@ex.automain
+def main(_run):
+    args = argparse.Namespace(**_run.config)
+
+    # Load data
+    train_loader, val_loader = load_data(args)
 
     # Define Network
     model = Net()
@@ -57,8 +98,9 @@ def main(_run):
 
     # Train the network
     for epoch in range(1, args.epochs + 1):  # loop over the dataset multiple times
+        model.train()
         args.current_epoch = epoch
-        loss_epoch = 0.
+        train_loss_epoch = 0.
 
         running_loss = 0.
         for i, data in enumerate(train_loader, 0):
@@ -76,19 +118,24 @@ def main(_run):
 
             # print statistics
             running_loss += loss.item()
-            loss_epoch += loss.item()
+            train_loss_epoch += loss.item()
             if i % 2000 == 1999:  # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                       (epoch, i + 1, running_loss / 2000))
                 step = epoch * steps_per_epoch + i + 1
-                _run.log_scalar("training.loss", running_loss / 2000, step)
-                writer.add_scalar("Loss/train", running_loss / 2000, step)
+                _run.log_scalar("training.loss.step", running_loss / 2000, step)
+                writer.add_scalar("Loss Steps/train", running_loss / 2000, step)
                 running_loss = 0.
 
         # Save model
         if epoch % args.checkpoint == 0:
             save_model(args, model)
+        # compute validation loss
+        val_loss_epoch = compute_val(model, val_loader, criterion)
+        _run.log_scalar("val.loss.epoch", val_loss_epoch)
+        _run.log_scalar("train.loss.epoch", train_loss_epoch / steps_per_epoch)
         writer.add_scalar("Learning_rate", args.lr, epoch)
-        writer.add_scalar("Loss Epoch/train", loss_epoch / steps_per_epoch, epoch)
+        writer.add_scalars("Loss vs Epoch", {"train_loss": train_loss_epoch / steps_per_epoch,
+                                             "val_loss": val_loss_epoch}, epoch)
 
     print('Finished Training')
