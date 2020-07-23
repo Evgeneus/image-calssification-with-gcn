ffeb1d736e807c50f593160c78ac8077c8bc047b
noname <MY_NAME@example.com>
2020-07-23 19:55:59 +0200

    fix/save model

--- src/utils.py
+++ src/utils.py
@@ -2,21 +2,21 @@ import torch
 from torch import nn
 
 
-def save_model(args, model):
+def save_model(args, model, msg):
     # To save a DataParallel model generically, save the model.module.state_dict().
     # This way, you have the flexibility to load the model any way you want to any device you want.
-    out = args.out_dir + "checkpoint_{}.tar".format(args.current_epoch)
+    if msg:
+        out = "../logs/classification/{}/pretrained/model_{}.tar".format(args.experiment_id, msg)
+    else:
+        out = "../logs/classification/{}/pretrained/checkpoint_{}.tar".format(args.experiment_id, args.current_epoch)
     if isinstance(model, torch.nn.DataParallel):
         torch.save(model.module.state_dict(), out)
     else:
         torch.save(model.state_dict(), out)
 
 
-def load_model(args, model, msg=None):
-    if msg:
-        model_path = "../logs/classification/{}/pretrained/model_{}.tar".format(args.experiment_id, msg)
-    else:
-        model_path = "../logs/classification/{}/pretrained/checkpoint_{}.tar".format(args.experiment_id, args.checkpoin_to_test)
+def load_model(args, model):
+    model_path = "../logs/classification/{}/pretrained/checkpoint_{}.tar".format(args.experiment_id, args.checkpoin_to_test)
     model.load_state_dict(torch.load(model_path))
 
     return model
